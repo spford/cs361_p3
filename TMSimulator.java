95c15e5545f360728571eea7500033a453a456e9
spencer <spencerford674@u.boisestate.edu>
2025-04-16 10:26:53 -0600

    Added practice files, Different organization

--- TMSimulator.java
+++ TMSimulator.java
@@ -4,10 +4,33 @@ import java.util.Scanner;
 
 public class TMSimulator {
 
-    protected static int numberOfStates;
-    protected static int startState = 0;
-    protected static int haltingState;
-    protected static int numbSymbols;
+    protected int numberOfStates;
+    protected int startState = 0;
+    protected int haltingState;
+    protected int numbSymbols;
+
+    public TMSimulator() {
+
+    }
+
+    private void fileParser(String fileName) {
+        try {
+            int currentLine = 0;
+            Scanner input = new Scanner(new FileInputStream(fileName));
+            while (input.hasNextLine()) {
+                if(currentLine == 0) {
+                    setStatesByNumber(Integer.parseInt(input.nextLine()));
+                } else if(currentLine == 1) {
+                    setSymbolsByNumber(Integer.parseInt(input.nextLine()));
+                } else {
+
+                }
+                currentLine++;
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
 
     private void setStatesByNumber(int numbStates) {
         numberOfStates = numbStates;
@@ -22,22 +45,14 @@ public class TMSimulator {
         }
     }
 
-    public void main(String[] args) {
+    public static void main(String[] args) {
 
-        try {
-            int currentLine = 0;
-            Scanner input = new Scanner(new FileInputStream(args[0]));
-            while (input.hasNextLine()) {
-                if(currentLine == 0) {
-                   setStatesByNumber(Integer.parseInt(input.nextLine()));
-                } else if(currentLine == 1) {
-                    setSymbolsByNumber(Integer.parseInt(input.nextLine()));
-                }
-                currentLine++;
-            }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
+        TMSimulator simulator = new TMSimulator();
+        simulator.fileParser(args[0]);
+        System.out.println("Number of states: " + simulator.numberOfStates);
+        System.out.println("Number of symbols: " + simulator.numbSymbols);
+        System.out.println("Halting state: " + simulator.haltingState);
+        System.out.println("Starting state: " + simulator.startState);
 
     }
 }
